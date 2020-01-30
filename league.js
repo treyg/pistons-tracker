3e4c2408f25cf2ad014f7507bbd226f6915238e4
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-01-30 18:31:53 -0500

    update news section

--- league.js
+++ league.js
@@ -5,7 +5,6 @@
   }
 
 
-
 //League News
 
 const LEAGUE_NEWS_URL = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news'
@@ -19,43 +18,43 @@ fetch(LEAGUE_NEWS_URL)
   const leagueNews = leagueNewsJSON.articles
   console.log(leagueNews)
 
-
-  // const stonsNews = newsData.filter(el => {
-  //    return el.categories.find(c => c.teamId == 8);
-  // })
-  
-
   const showLeagueNews = () => {
       let leagueNewsSection = document.getElementById('leagueNewsSection')
       
       for (let i = 0; i < 6; i++) {
       
-      let imgContainer = document.createElement('div')
-        leagueNewsSection.appendChild(imgContainer)
-        imgContainer.classList.add('flex', 'flex-col', 'py-2', 'overflow-hidden', 'mb-6', 'border-2', 'border-gray-200', 'rounded')
+      let leagueImgContainer = document.createElement('div')
+        leagueNewsSection.appendChild(leagueImgContainer)
+        leagueImgContainer.classList.add('flex', 'flex-col', 'py-2', 'overflow-hidden', 'mb-6', 'border-2', 'border-gray-200', 'rounded', 'cursor-pointer')
 
         let leagueNewsImage = document.createElement('img')
         leagueNewsImage.src = `${leagueNews[i].images[0].url}`
-        leagueNewsImage.classList.add('ml-3', 'rounded-sm' )
-        imgContainer.appendChild(leagueNewsImage)
+        leagueNewsImage.classList.add('ml-3', 'rounded-sm')
+        leagueImgContainer.appendChild(leagueNewsImage)
 
         let leagueNewsTitle = document.createElement('div')
         leagueNewsTitle.textContent = `${leagueNews[i].headline}`
-        leagueNewsTitle.classList.add('mx-3', 'text-base')
-        imgContainer.appendChild(leagueNewsTitle)
+        leagueNewsTitle.classList.add('mx-3', 'text-base', 'mt-2', 'font-medium')
+        leagueImgContainer.appendChild(leagueNewsTitle)
 
         let leagueNewsDescription = document.createElement('div')
         leagueNewsDescription.textContent = `${leagueNews[i].description}`
-        imgContainer.appendChild(leagueNewsDescription)
+        leagueNewsDescription.classList.add('text-sm', 'mx-3', 'mt-2')
+        leagueImgContainer.appendChild(leagueNewsDescription)
 
         let leagueNewsProvider = document.createElement('p')
         leagueNewsProvider.textContent = `${leagueNews[i].images[0].credit}`
         leagueNewsProvider.classList.add('text-xs', 'text-gray-800', 'pt-1')
         leagueNewsDescription.appendChild(leagueNewsProvider)
 
-        // imgContainer.addEventListener('click', () => {
-        //     window.open(leagueNews[i].ampUrl)
-        // })
+        if (leagueNewsProvider.textContent == undefined) {
+          leagueNewsProvider.textContent.classList.add('hidden')
+        } 
+     
+
+        leagueImgContainer.addEventListener('click', () => {
+            window.open(leagueNews[i].links.web.short.href)
+        })
       
         }
 
