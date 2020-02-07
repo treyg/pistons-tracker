d65cd8a0bf1abba3d1d511f133be02421afc23a2
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-02-06 19:41:19 -0500

    update last 5 games styling

--- app.js
+++ app.js
@@ -248,13 +248,16 @@ fetch(PISTONS_GAMES_URL)
             completedStonsGames[i].visitor_team_score
         ) {
           homeScore.innerHTML = `
-          ${completedStonsGames[i].home_team_score}<span class="text-black-500 font-medium mr-2"><</span>`;
-          visitorScore.classList.add('pr-5')
+          ${completedStonsGames[i].home_team_score}<span class="text-black-500 font-medium "></span>`;
+          visitorScore.classList.add('text-gray-600')
+          visitorName.classList.add('text-gray-600')
+
+          
 
         } else if ( completedStonsGames[i].home_team_score <
           completedStonsGames[i].visitor_team_score) {
-          visitorScore.innerHTML = `${completedStonsGames[i].home_team_score}<span class="text-black-500 font-medium mr-2"><</span> `;
-          homeScore.classList.add('pr-5')
+          visitorScore.innerHTML = `${completedStonsGames[i].home_team_score}<span class="text-black-500 font-medium"></span> `;
+          homeScore.classList.add()
         }
 
 
