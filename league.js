60994a204e789932818b2535ecd8f4a579f179b3
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-02-10 15:58:55 -0500

    update live games section

--- league.js
+++ league.js
@@ -16,7 +16,6 @@ fetch(LEAGUE_GAMES_TODAY)
     const gamesToday = gamesTodayJSON;
     //Set variable for just games today
     const eventsToday = gamesToday.events;
-    console.log(eventsToday);
 
     for (let i = 0; i < eventsToday.length; i++) {
       let gamesToday = document.getElementById("gamesToday");
@@ -44,7 +43,13 @@ fetch(LEAGUE_GAMES_TODAY)
       gameTime.innerHTML = `Today <br> ${dayjs(
         eventsToday[i].competitions[0].date
       ).format("h:mm a")}`;
-      gameTime.classList.add('border-l',"text-xs", "text-center", 'px-2', 'w-16');
+      gameTime.classList.add(
+        "border-l",
+        "text-xs",
+        "text-center",
+        "px-2",
+        "w-16"
+      );
       statusArea.appendChild(gameTime);
 
       let homeTeamDiv = document.createElement("div");
@@ -79,107 +84,122 @@ fetch(LEAGUE_GAMES_TODAY)
 
       singleGameArea.appendChild(statusArea);
 
-      console.log(eventsToday)
+      //If status of the game is such that game has started, add the score section for home and away, along with quarter and time
+      if (
+        eventsToday[i].competitions[0].status != undefined &&
+        eventsToday[i].competitions[0].status.period > 0
+      ) {
+        let homeScore = document.createElement("span");
+        homeScore.textContent =
+          eventsToday[i].competitions[0].competitors[0].score;
+        homeTeamDiv.appendChild(homeScore);
+        homeScore.classList.add("ml-auto", "mr-3");
 
-       if (eventsToday[i].competitions[0].status != undefined  && eventsToday[i].competitions[0].status.period > 0) {
-          let homeScore = document.createElement('span')
-              homeScore.textContent = eventsToday[i].competitions[0].competitors[0].score
-              homeTeamDiv.appendChild(homeScore)
-              homeScore.classList.add('ml-auto', 'mr-3')
+        let awayScore = document.createElement("span");
+        awayScore.textContent =
+          eventsToday[i].competitions[0].competitors[1].score;
+        awayTeamDiv.appendChild(awayScore);
+        awayScore.classList.add("ml-auto", "mr-3");
 
-              let awayScore = document.createElement('span')
-              awayScore.textContent = eventsToday[i].competitions[0].competitors[1].score
-              awayTeamDiv.appendChild(awayScore)
-              awayScore.classList.add('ml-auto', 'mr-3')
+        gameTime.innerHTML = `<span class="text-sm text-black">Q: ${eventsToday[i].status.period}</span><br>${eventsToday[i].status.displayClock}`;
+        gameTime.classList.add("text-base", "text-green-500");
+      }
+      //If period = 5, set html to show OT
+      if (eventsToday[i].status.period === 5) {
+        gameTime.innerHTML = `<span class="text-sm text-black">OT`;
+      }
 
-
-          gameTime.innerHTML = `<span class="text-sm text-black">Q: ${eventsToday[i].status.period}</span><br>${eventsToday[i].status.displayClock}`
-          gameTime.classList.add('text-base', 'text-green-500')
-       }
+      if (eventsToday[i].status.period === 6) {
+        gameTime.innerHTML = `<span class="text-sm text-black">OT:2`;
+      }
+      //If game is completed set inner hmtl to final
+      if (eventsToday[i].status.type.completed === true) {
+        gameTime.innerHTML = `Final`;
+      }
     }
 
-
+    console.log(eventsToday);
   });
 
 //League News
 
 const LEAGUE_NEWS_URL =
-  "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news";
+  //"https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news";
 
-fetch(LEAGUE_NEWS_URL)
-  .then(response => {
-    return response.json();
-  })
-  .then(leagueNewsJSON => {
-    //Set the JSON data for games to a variable
-    const leagueNews = leagueNewsJSON.articles;
-    // console.log(leagueNews);
+  fetch(LEAGUE_NEWS_URL)
+    .then(response => {
+      return response.json();
+    })
+    .then(leagueNewsJSON => {
+      //Set the JSON data for games to a variable
+      const leagueNews = leagueNewsJSON.articles;
+      // console.log(leagueNews);
 
-    //Sort league news to show newest stories first
-    const sortedLeagueNews = leagueNews.sort((a, b) =>
-      dayjs(a.categories[0].createDate).format("H:MM") >
-      dayjs(b.categories[0].createDate).format("H:MM")
-        ? 1
-        : -1
-    );
+      //Sort league news to show newest stories first
+      const sortedLeagueNews = leagueNews.sort((a, b) =>
+        dayjs(a.categories[0].createDate).format("H:MM") >
+        dayjs(b.categories[0].createDate).format("H:MM")
+          ? 1
+          : -1
+      );
 
-    const showLeagueNews = () => {
-      let leagueNewsSection = document.getElementById("leagueNewsSection");
-      //Loop through leagueNews JSON data and create sections for each story
-      for (let i = 0; i < 6; i++) {
-        let leagueImgContainer = document.createElement("div");
-        leagueNewsSection.appendChild(leagueImgContainer);
-        leagueImgContainer.classList.add(
-          "flex",
-          "flex-col",
-          "py-2",
-          "overflow-hidden",
-          "mb-6",
-          "border-2",
-          "border-gray-200",
-          "rounded",
-          "cursor-pointer"
-        );
+      const showLeagueNews = () => {
+        let leagueNewsSection = document.getElementById("leagueNewsSection");
+        //Loop through leagueNews JSON data and create sections for each story
+        for (let i = 0; i < 6; i++) {
+          let leagueImgContainer = document.createElement("div");
+          leagueNewsSection.appendChild(leagueImgContainer);
+          leagueImgContainer.classList.add(
+            "flex",
+            "flex-col",
+            "py-2",
+            "overflow-hidden",
+            "mb-6",
+            "border-2",
+            "border-gray-200",
+            "rounded",
+            "cursor-pointer"
+          );
 
-        let leagueNewsImage = document.createElement("img");
-        leagueNewsImage.src = `${sortedLeagueNews[i].images[0].url}`;
-        leagueNewsImage.classList.add("mx-3", "rounded-sm");
-        leagueImgContainer.appendChild(leagueNewsImage);
-        //If no image is given for story, leave image blank and continue loop
-        if (leagueNews[i].images[0].url == undefined) {
-          leagueNewsImage.src = ``;
+          let leagueNewsImage = document.createElement("img");
+          leagueNewsImage.src = `${sortedLeagueNews[i].images[0].url}`;
+          leagueNewsImage.classList.add("mx-3", "rounded-sm");
+          leagueImgContainer.appendChild(leagueNewsImage);
+          //If no image is given for story, leave image blank and continue loop
+          if (leagueNews[i].images[0].url == undefined) {
+            leagueNewsImage.src = ``;
+          }
+
+          let leagueNewsTitle = document.createElement("div");
+          leagueNewsTitle.textContent = `${sortedLeagueNews[i].headline}`;
+          leagueNewsTitle.classList.add(
+            "mx-3",
+            "text-base",
+            "mt-2",
+            "font-medium"
+          );
+          leagueImgContainer.appendChild(leagueNewsTitle);
+
+          let leagueNewsDescription = document.createElement("div");
+          leagueNewsDescription.textContent = `${sortedLeagueNews[i].description}`;
+          leagueNewsDescription.classList.add("text-sm", "mx-3", "mt-2");
+          leagueImgContainer.appendChild(leagueNewsDescription);
+
+          let leagueNewsProvider = document.createElement("p");
+          leagueNewsProvider.textContent = `${sortedLeagueNews[i].images[0].credit}`;
+          leagueNewsProvider.classList.add("text-xs", "text-gray-800", "pt-1");
+          leagueNewsDescription.appendChild(leagueNewsProvider);
+
+          //Default league news source to espn if no credit is given
+          if (leagueNews[i].images[0].credit == undefined) {
+            leagueNewsProvider.innerHTML = `ESPN`;
+          }
+
+          leagueImgContainer.addEventListener("click", () => {
+            window.open(leagueNews[i].links.web.short.href);
+          });
         }
+      };
 
-        let leagueNewsTitle = document.createElement("div");
-        leagueNewsTitle.textContent = `${sortedLeagueNews[i].headline}`;
-        leagueNewsTitle.classList.add(
-          "mx-3",
-          "text-base",
-          "mt-2",
-          "font-medium"
-        );
-        leagueImgContainer.appendChild(leagueNewsTitle);
-
-        let leagueNewsDescription = document.createElement("div");
-        leagueNewsDescription.textContent = `${sortedLeagueNews[i].description}`;
-        leagueNewsDescription.classList.add("text-sm", "mx-3", "mt-2");
-        leagueImgContainer.appendChild(leagueNewsDescription);
-
-        let leagueNewsProvider = document.createElement("p");
-        leagueNewsProvider.textContent = `${sortedLeagueNews[i].images[0].credit}`;
-        leagueNewsProvider.classList.add("text-xs", "text-gray-800", "pt-1");
-        leagueNewsDescription.appendChild(leagueNewsProvider);
-
-        //Default league news source to espn if no credit is given
-        if (leagueNews[i].images[0].credit == undefined) {
-          leagueNewsProvider.innerHTML = `ESPN`;
-        }
-
-        leagueImgContainer.addEventListener("click", () => {
-          window.open(leagueNews[i].links.web.short.href);
-        });
-      }
-    };
-
-    showLeagueNews();
-  });
+      showLeagueNews();
+    });
