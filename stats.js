223e481dbec6f4634866d0273e620dae818e4143
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-02-11 15:47:30 -0500

    test player averages endpoint

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
-const PISTONS_STATS = 'https://www.balldontlie.io/api/v1/players?search=trey'
-
+const PISTONS_STATS = 'https://www.balldontlie.io/api/v1/season_averages?season=2019&player_ids[]=10&player_ids[]=20'
+//'https://www.balldontlie.io/api/v1/players?year=[]2019?per_page=100'
 //'https://www.balldontlie.io/api/v1/stats?seasons=[]2019&player_ids[]=1&per_page=100'
 
   fetch(PISTONS_STATS)
@@ -8,9 +8,18 @@ const PISTONS_STATS = 'https://www.balldontlie.io/api/v1/players?search=trey'
   })
   .then((statsJSON) => {
     //Set the JSON data for games to a variable
-    const stats = statsJSON.data
+    const stats = statsJSON
     console.log(stats)
 
-    const pistonsStats = stats.filter(all => all.team.id === 9)
-        console.log(pistonsStats)
-  });
+//     const sortedStats = stats.sort((a, b) =>
+//     a.team.city > b.team.city ? 1 : -1
+//   );
+
+//   console.log(sortedStats)
+ 
+//   const filteredStats = sortedStats.filter(players => {
+//     return players.team.city.includes('Detroit')
+//   })
+// console.table(filteredStats)
+
+  })
