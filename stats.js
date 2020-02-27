f71e968b2e6279326f4e187a8d37138a9fa6e4a5
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-02-27 16:44:12 -0500

    add three point and rebound sections

--- stats.js
+++ stats.js
@@ -44,9 +44,15 @@ fetch(PISTONS_STATS)
 
     const sekouDoumbouya = stats.find(e => e.player_id == 666543);
     sekouDoumbouya.name = "Sekou Doumbouya";
+    sekouDoumbouya.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629635.png";
 
     const sviMykhailiuk = stats.find(e => e.player_id == 338);
     sviMykhailiuk.name = "Svi Mykhailiuk";
+    sviMykhailiuk.imgOne =
+      "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/1629004.png";
+    sviMykhailiuk.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629004.png";
 
     const bruceBrown = stats.find(e => e.player_id == 69);
     bruceBrown.name = "Bruce Brown";
@@ -57,34 +63,61 @@ fetch(PISTONS_STATS)
 
     const thonMaker = stats.find(e => e.player_id == 295);
     thonMaker.name = "Thon Maker";
-
-    const markieffMorris = stats.find(e => e.player_id == 329);
-    markieffMorris.name = "Markieff Morris";
+    thonMaker.imgOne =
+      "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/1627748.png";
+    thonMaker.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1627748.png";
 
     const langstonGalloway = stats.find(e => e.player_id == 168);
     langstonGalloway.name = "Langston Galloway";
+    langstonGalloway.imgOne =
+      "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/204038.png";
+    langstonGalloway.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/204038.png";
 
     const brandonKnight = stats.find(e => e.player_id == 258);
     brandonKnight.name = "Brandon Knight";
+    brandonKnight.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/202688.png";
 
     const tonySnell = stats.find(e => e.player_id == 426);
     tonySnell.name = "Tony Snell";
+    tonySnell.imgOne =
+      "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/203503.png";
+    tonySnell.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203503.png";
 
     const louisKing = stats.find(e => e.player_id == 666698);
     louisKing.name = "Louis King";
+    louisKing.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629663.png";
 
     const jordanBone = stats.find(e => e.player_id == 666454);
     jordanBone.name = "Jordan Bone";
+    jordanBone.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629648.png";
 
     const khyriThomas = stats.find(e => e.player_id == 441);
     khyriThomas.name = "Khyri Thomas";
+    khyriThomas.imgTwo =
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629017.png";
 
     const johnHenson = stats.find(e => e.player_id == 205);
     johnHenson.name = "John Henson";
     johnHenson.imgOne =
       "https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/203089.png";
     johnHenson.imgTwo =
-      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203089.png";
+      "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203089.png ";
+
+    // const dontaHall = stats.find(e => e.player_id == 666611);
+    // dontaHall.name = "Donta Hall";
+    // dontaHall.imgTwo =
+    //   "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629743.png";
+
+    // const derrickWaltonJr = stats.find(e => e.player_id == 2208);
+    // derrickWaltonJr.name = "Derrick Walton Jr.";
+    // derrickWaltonJr.imgTwo =
+    //   "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1629743.png";
 
     //const sortedByAssists = stats.sort((a, b) => (a.ast < b.ast ? 1 : -1));
 
@@ -100,7 +133,7 @@ fetch(PISTONS_STATS)
       const pointsLeaderInfo = document.createElement("div");
       pointsLeaderInfo.classList.add("mt-6");
       const pointsLeaderImg = document.createElement("img");
-      pointsLeaderImg.classList.add("w-8/12", "overflow-hidden");
+      pointsLeaderImg.classList.add("w-8/12", "h-auto");
       pointsLeaderImg.src = sortedByPoints[0].imgOne;
 
       leadersSection.appendChild(pointsLeaderSection);
@@ -115,7 +148,7 @@ fetch(PISTONS_STATS)
       );
 
       pointsLeaderInfo.innerHTML = `
-      <span class="text-4xl font-semibold">${sortedByPoints[0].pts.toFixed()}</span>
+      <span class="text-4xl font-semibold">${sortedByPoints[0].pts.toFixed(1)}</span>
         <p>${sortedByPoints[0].name}</P>
         `;
 
@@ -162,7 +195,7 @@ fetch(PISTONS_STATS)
       const assistsLeaderInfo = document.createElement("div");
       assistsLeaderInfo.classList.add("mt-6");
       const assistsLeaderImg = document.createElement("img");
-      assistsLeaderImg.classList.add("w-8/12", "overflow-hidden");
+      assistsLeaderImg.classList.add("w-8/12", "h-auto");
       assistsLeaderImg.src = sortedByassists[0].imgOne;
 
       leadersSectionTwo.appendChild(assistsLeaderSection);
@@ -177,7 +210,7 @@ fetch(PISTONS_STATS)
       );
 
       assistsLeaderInfo.innerHTML = `
-      <span class="text-4xl font-semibold">${sortedByassists[0].ast.toFixed()}</span>
+      <span class="text-4xl font-semibold">${sortedByassists[0].ast.toFixed(1)}</span>
         <p>${sortedByassists[0].name}</P>
         `;
 
@@ -226,7 +259,7 @@ fetch(PISTONS_STATS)
       const reboundsLeaderInfo = document.createElement("div");
       reboundsLeaderInfo.classList.add("mt-6");
       const reboundsLeaderImg = document.createElement("img");
-      reboundsLeaderImg.classList.add("w-8/12", "overflow-hidden");
+      reboundsLeaderImg.classList.add("w-8/12", "h-auto");
       reboundsLeaderImg.src = sortedByrebounds[0].imgOne;
 
       leadersSectionThree.appendChild(reboundsLeaderSection);
@@ -241,7 +274,7 @@ fetch(PISTONS_STATS)
       );
 
       reboundsLeaderInfo.innerHTML = `
-      <span class="text-4xl font-semibold">${sortedByrebounds[0].reb.toFixed()}</span>
+      <span class="text-4xl font-semibold">${sortedByrebounds[0].reb.toFixed(1)}</span>
         <p>${sortedByrebounds[0].name}</P>
         `;
 
@@ -275,4 +308,132 @@ fetch(PISTONS_STATS)
     };
 
     showRebounds();
+
+    //Section for showing FG percentage
+
+    const showFGP = () => {
+      const sortedByFGP = stats.sort((a, b) => (a.fg_pct < b.fg_pct ? 1 : -1));
+
+      const leadersSectionFour = document.getElementById("leadersSectionFour");
+
+      const FGPLeaderSection = document.createElement("div");
+      FGPLeaderSection.classList.add("pb-4");
+      const FGPLeaderInfo = document.createElement("div");
+      FGPLeaderInfo.classList.add("mt-6");
+      const FGPLeaderImg = document.createElement("img");
+      FGPLeaderImg.classList.add("w-8/12", "h-auto");
+      FGPLeaderImg.src = sortedByFGP[0].imgOne;
+
+      leadersSectionFour.appendChild(FGPLeaderSection);
+      FGPLeaderSection.appendChild(FGPLeaderInfo);
+      FGPLeaderSection.appendChild(FGPLeaderImg);
+
+      FGPLeaderSection.classList.add(
+        "flex",
+        "flex-row",
+        "justify-between",
+        "px-4"
+      );
+
+      FGPLeaderInfo.innerHTML = `
+      <span class="text-4xl font-semibold">${(
+        sortedByFGP[0].fg_pct * 100
+      ).toFixed(1)}</span>
+        <p>${sortedByFGP[0].name}</P>
+        `;
+
+      const runnersUpSection = document.createElement("div");
+      runnersUpSection.classList.add("px-4", "py-4", "flex", "justify-between");
+
+      leadersSectionFour.appendChild(runnersUpSection);
+
+      for (let i = 1; i < 4; i++) {
+        const runnersUp = document.createElement("div");
+        runnersUp.classList.add("w-4/12");
+        const runnersUpImg = document.createElement("img");
+        runnersUpImg.src = sortedByFGP[i].imgTwo;
+
+        //console.log(runnersUpImg);
+
+        runnersUp.innerHTML = `
+        <span class = "text-sm">${sortedByFGP[i].name}</span>
+          <br>
+            <span class="font-semibold">${(sortedByFGP[i].fg_pct * 100).toFixed(
+              1
+            )}<img src=${
+          sortedByFGP[i].imgTwo
+        } class="w-16 ml-auto -mt-4"></img></span>
+        `;
+
+        runnersUpSection.appendChild(runnersUp);
+      }
+
+      // console.log(sortedByFGP);
+    };
+
+    showFGP();
+
+    //Three point percentage section
+
+    const showTPP = () => {
+      const sortedByTPP = stats.sort((a, b) => (a.fg3_pct < b.fg3_pct ? 1 : -1));
+
+      const leadersSectionFive = document.getElementById("leadersSectionFive");
+
+      const TPPLeaderSection = document.createElement("div");
+      TPPLeaderSection.classList.add("pb-4");
+      const TPPLeaderInfo = document.createElement("div");
+      TPPLeaderInfo.classList.add("mt-6");
+      const TPPLeaderImg = document.createElement("img");
+      TPPLeaderImg.classList.add("w-8/12", "h-auto");
+      TPPLeaderImg.src = sortedByTPP[0].imgOne;
+
+      leadersSectionFive.appendChild(TPPLeaderSection);
+      TPPLeaderSection.appendChild(TPPLeaderInfo);
+      TPPLeaderSection.appendChild(TPPLeaderImg);
+
+      TPPLeaderSection.classList.add(
+        "flex",
+        "flex-row",
+        "justify-between",
+        "px-4"
+      );
+
+      TPPLeaderInfo.innerHTML = `
+      <span class="text-4xl font-semibold">${(
+        sortedByTPP[0].fg_pct * 100
+      ).toFixed(1)}</span>
+        <p>${sortedByTPP[0].name}</P>
+        `;
+
+      const runnersUpSection = document.createElement("div");
+      runnersUpSection.classList.add("px-4", "py-4", "flex", "justify-between");
+
+      leadersSectionFive.appendChild(runnersUpSection);
+
+      for (let i = 1; i < 4; i++) {
+        const runnersUp = document.createElement("div");
+        runnersUp.classList.add("w-4/12");
+        const runnersUpImg = document.createElement("img");
+        runnersUpImg.src = sortedByTPP[i].imgTwo;
+
+        //console.log(runnersUpImg);
+
+        runnersUp.innerHTML = `
+        <span class = "text-sm">${sortedByTPP[i].name}</span>
+          <br>
+            <span class="font-semibold">${(sortedByTPP[i].fg_pct * 100).toFixed(
+              1
+            )}<img src=${
+          sortedByTPP[i].imgTwo
+        } class="w-16 ml-auto -mt-4"></img></span>
+        `;
+
+        runnersUpSection.appendChild(runnersUp);
+      }
+
+      // console.log(sortedByTPP);
+    };
+
+    showTPP();
   });
