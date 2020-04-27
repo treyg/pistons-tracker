c8919724965467844dea9f1f2e012b9725a46c67
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-04-27 19:04:13 -0400

    update css

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,8 @@ module.exports = {
                 'public/index.html',
                 'public/nbatoday.html',
                 'public/podcast.html',
-                'public/stats.html'
+                'public/stats.html',
+                'public/js/app.js'
 
             ],
             defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
