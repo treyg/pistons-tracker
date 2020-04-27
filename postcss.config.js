8c4464cbf68a016e0ac0171e2cb0779bdb89e95c
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-04-27 18:54:14 -0400

    add postcss for tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,15 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                'public/index.html',
+                'public/nbatoday.html',
+                'public/podcast.html',
+                'public/stats.html'
+
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
