4d8750d26a46a5624deb88df84bf3820f59a4443
treyg21 <41927921+treyg21@users.noreply.github.com>
2020-04-28 16:09:39 -0400

    test other news endpoint

--- index.js
+++ index.js
@@ -52,8 +52,9 @@ const requestOptions = {
   },
 };
 
-const api_url =
-  "https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=detroit+pistons";
+const api_url = "https://pistons-news.cognitiveservices.azure.com/bing/v7.0"
+  //"https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=detroit+pistons";
+
 
 // 
 
