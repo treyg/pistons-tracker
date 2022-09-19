701ea4e4ece9d489f2f7605183a00aab94e93c39
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-09-19 16:30:29 -0400

    update gitignore

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const Loader = () => {
   let circleCommonClasses = "h-2.5 w-2.5 bg-current   rounded-full";
-
   return (
     <div className="absolute left-1/2 top-1/2 flex">
       <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
