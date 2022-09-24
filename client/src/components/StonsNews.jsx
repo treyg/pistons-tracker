4519cd79bee6deea5f7f458040fdc4eb413958a3
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-09-24 11:50:58 -0400

    add stons news

--- client/src/components/StonsNews.jsx
+++ client/src/components/StonsNews.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loader from "./Loader";
+import { ref, onValue } from "firebase/database";
+import db from "../api/firebase";
+import StonsNewsItem from "./StonsNewsItem";
 
 const StonsNews = () => {
-  return <div>StonsNews</div>;
+  const [news, setFullNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNews = () => {
+      setLoading(true);
+      const newsRef = ref(db, "currentNews/value");
+      onValue(newsRef, (snapshot) => {
+        const fullNews = snapshot.val();
+        setFullNews(fullNews);
+        setLoading(false);
+      });
+    };
+    fetchNews();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="mx-3 my-4 flex flex-col rounded py-4 px-4 shadow-md">
+          <h2 className="mb-5 font-bold">Stons News</h2>
+          {news.map((article) => (
+            <StonsNewsItem
+              key={article.id}
+              title={article.name}
+              author={article.provider[0].name}
+              datePublished={article.datePublished}
+              image={article.image?.thumbnail?.contentUrl}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default StonsNews;
