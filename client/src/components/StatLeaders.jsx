c74143d94cbeea1470fd8b4ce5083d12686998dc
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-09-27 20:28:07 -0400

    Update home layout

--- client/src/components/StatLeaders.jsx
+++ client/src/components/StatLeaders.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-//import { useEffect, useState } from "react";
+import React from "react";
 import Leader from "./Leader";
 import RunnerUp from "./RunnerUp";
 
