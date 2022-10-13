61713c6dc1b76bb0c6ded7ac58688abd4af7eef8
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-10-12 20:07:01 -0400

    Fetch news data from azure and save to firebase

--- server/firebase.js
+++ server/firebase.js
@@ -4,10 +4,11 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import { getDatabase, ref, set, get } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDz-XuB2IrPKc8FzYzrw8NWdJt9UokVcu0",
@@ -36,4 +37,35 @@ const playerData = get(ref(database, 'roster')).then(snapshot => {
     return data.players;
 })
 
-export { updateRoster, playerData };
+//Function to fetch news from azure and save to firebase
+const api_url =
+    "https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=detroit+pistons";
+
+const api_key = process.env.API_KEY;
+const requestOptions = {
+    headers: {
+        "Ocp-Apim-Subscription-Key": api_key,
+    },
+};
+
+
+const getNews = () => {
+    fetch(api_url, requestOptions)
+        .then((res) => res.json())
+        .then((data) => {
+
+            const news = data.value;
+            console.log(data)
+            set(ref(database, 'news'), {
+                articles: news
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+
+
+
+export { updateRoster, playerData, getNews };
--- server/index.js
+++ server/index.js
@@ -1,74 +1,80 @@
+import fetch from "node-fetch";
+import { scrapeRoster } from "./scrape.js";
+import { updateRoster, playerData, getNews } from "./firebase.js";
 
-import fetch from 'node-fetch';
-import { scrapeRoster } from './scrape.js';
-import { updateRoster, playerData } from './firebase.js';
+import schedule from "node-schedule";
 
-import schedule from 'node-schedule';
+//schedule job to run every 4 hours
+schedule.scheduleJob("0 */4 * * *", () => {
+    async function runIndex() {
+        scrapeRoster();
 
-async function runIndex() {
-    scrapeRoster();
+        const rosterObj = await playerData;
 
-    const rosterObj = await playerData
-
-    const pushPlayerInfo = async () => {
-        const buildRoster = async () => {
-            Object.entries(rosterObj).forEach(([key, value]) => {
-                const sanitizedName = value?.name.replace(/\s/g, '%20').toLowerCase();
-                rosterObj[key].name = sanitizedName;
-            });
-        }
-
-        await buildRoster();
-
-        const getPlayerIds = async () => {
-            const playerNames = Object.keys(rosterObj);
-            const playerIds = [];
-            for (let i = 0; i < playerNames.length; i++) {
-                const response = await fetch(`https://www.balldontlie.io/api/v1/players?search=${playerNames[i]}`);
-                const data = await response.json();
-                const playerId = data.data[0]?.id;
-                const position = data.data[0]?.position;
-                const weight = data.data[0]?.weight_pounds;
-                const height = `${data.data[0]?.height_feet}'${data.data[0]?.height_inches}`;
-                rosterObj[playerNames[i]].id = playerId || 0;
-                rosterObj[playerNames[i]].position = position || 'N/A';
-                rosterObj[playerNames[i]].weight = weight || 'N/A';
-                rosterObj[playerNames[i]].height = height || 'N/A';
-
-                playerId ? playerIds.push(playerId) : null
-            }
-
-            //Use array of playerIds to get player stats from balldontlie API
-            const idString = playerIds.join("&player_ids[]=", "");
-            await fetch(
-                `https://www.balldontlie.io/api/v1/season_averages?season=${new Date().getFullYear() - 1
-                }&player_ids[]=${idString}`
-            )
-                .then((res) => res.json())
-                .then((data) => {
-                    const playerStats = data.data;
-                    for (let i = 0; i < playerStats.length; i++) {
-                        //find the player in the roster object with the same player_id and add the stats to the object
-                        Object.entries(rosterObj).forEach(([key, value]) => {
-                            if (value.id === playerStats[i].player_id) {
-                                rosterObj[key].stats = playerStats[i];
-                            }
-                        })
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
+        const pushPlayerInfo = async () => {
+            const buildRoster = async () => {
+                Object.entries(rosterObj).forEach(([key, value]) => {
+                    const sanitizedName = value?.name.replace(/\s/g, "%20").toLowerCase();
+                    rosterObj[key].name = sanitizedName;
                 });
+            };
 
-        }
-        await getPlayerIds();
-        updateRoster(rosterObj);
+            await buildRoster();
+
+            const getPlayerIds = async () => {
+                const playerNames = Object.keys(rosterObj);
+                const playerIds = [];
+                for (let i = 0; i < playerNames.length; i++) {
+                    const response = await fetch(
+                        `https://www.balldontlie.io/api/v1/players?search=${playerNames[i]}`
+                    );
+                    const data = await response.json();
+                    const playerId = data.data[0]?.id;
+                    const position = data.data[0]?.position;
+                    const weight = data.data[0]?.weight_pounds;
+                    const height = `${data.data[0]?.height_feet}'${data.data[0]?.height_inches}`;
+                    rosterObj[playerNames[i]].id = playerId || 0;
+                    rosterObj[playerNames[i]].position = position || "N/A";
+                    rosterObj[playerNames[i]].weight = weight || "N/A";
+                    rosterObj[playerNames[i]].height = height || "N/A";
+
+                    playerId ? playerIds.push(playerId) : null;
+                }
+
+                //Use array of playerIds to get player stats from balldontlie API
+                const idString = playerIds.join("&player_ids[]=", "");
+                await fetch(
+                    `https://www.balldontlie.io/api/v1/season_averages?season=${new Date().getFullYear() - 1
+                    }&player_ids[]=${idString}`
+                )
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const playerStats = data.data;
+                        for (let i = 0; i < playerStats.length; i++) {
+                            Object.entries(rosterObj).forEach(([key, value]) => {
+                                if (value.id === playerStats[i].player_id) {
+                                    rosterObj[key].stats = playerStats[i];
+                                }
+                            });
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            };
+            await getPlayerIds();
+            updateRoster(rosterObj);
+        };
+
+        await pushPlayerInfo();
+
+        console.log("The answer to life, the universe, and everything!");
     }
+    runIndex();
+});
 
-    await pushPlayerInfo();
 
-
-    console.log('The answer to life, the universe, and everything!');
-}
-runIndex();
-
+//schedule job to run every hour
+schedule.scheduleJob("0 * * * *", () => {
+    getNews()
+})
