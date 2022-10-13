37e5815cd8363334acdd8be963efa2f46b7a188a
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-10-13 16:33:17 -0400

    Add https to espn api

--- client/src/api/getNextFiveStons.js
+++ client/src/api/getNextFiveStons.js
@@ -1,7 +1,7 @@
 
 
 const getNextFiveStons = async () => {
-    const response = await fetch(`http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/08/schedule`);
+    const response = await fetch(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/08/schedule`);
     const data = await response.json();
     return data;
 }
