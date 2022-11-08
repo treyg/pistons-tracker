8a03fbfb6317460303daa307804063a048dd800e
Trey Gordon <41927921+treyg@users.noreply.github.com>
2022-11-07 19:23:44 -0500

    Start PWA config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "StonsCenter",
+        short_name: "StonsCenter",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#041e42",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#041e42",
+      },
+    }),
+  ],
+});
