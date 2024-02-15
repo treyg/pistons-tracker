9dc9dff82519b798153cbbbdbcad4731f311cd78
Trey Gordon <41927921+treyg@users.noreply.github.com>
2024-02-15 14:07:48 -0500

    add basic express server

--- server/index.js
+++ server/index.js
@@ -1,73 +1,87 @@
-import fetch from 'node-fetch'
-import { scrapeRoster } from './scrape.js'
-import { updateRoster, playerData, getNews } from './firebase.js'
+import fetch from "node-fetch";
+import { scrapeRoster } from "./scrape.js";
+import { updateRoster, playerData, getNews } from "./firebase.js";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Health check route
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 async function runIndex() {
-  console.log('Scraping Roster')
-  scrapeRoster()
-  console.log('Updating Roster')
-  const rosterObj = await playerData
+  console.log("Scraping Roster");
+  scrapeRoster();
+  console.log("Updating Roster");
+  const rosterObj = await playerData;
 
   const pushPlayerInfo = async () => {
     const buildRoster = async () => {
       Object.entries(rosterObj).forEach(([key, value]) => {
-        const sanitizedName = value?.name.replace(/\s/g, '%20').toLowerCase()
-        rosterObj[key].name = sanitizedName
-      })
-    }
+        const sanitizedName = value?.name.replace(/\s/g, "%20").toLowerCase();
+        rosterObj[key].name = sanitizedName;
+      });
+    };
 
-    await buildRoster()
+    await buildRoster();
 
     const getPlayerIds = async () => {
-      const playerNames = Object.keys(rosterObj)
-      const playerIds = []
+      const playerNames = Object.keys(rosterObj);
+      const playerIds = [];
       for (let i = 0; i < playerNames.length; i++) {
         const response = await fetch(
           `https://www.balldontlie.io/api/v1/players?search=${playerNames[i]}`
-        )
-        const data = await response.json()
-        const playerId = data.data[0]?.id
-        const position = data.data[0]?.position
-        const weight = data.data[0]?.weight_pounds
-        const height = `${data.data[0]?.height_feet}'${data.data[0]?.height_inches}`
-        rosterObj[playerNames[i]].id = playerId || 0
-        rosterObj[playerNames[i]].position = position || 'N/A'
-        rosterObj[playerNames[i]].weight = weight || 'N/A'
-        rosterObj[playerNames[i]].height = height || 'N/A'
+        );
+        const data = await response.json();
+        const playerId = data.data[0]?.id;
+        const position = data.data[0]?.position;
+        const weight = data.data[0]?.weight_pounds;
+        const height = `${data.data[0]?.height_feet}'${data.data[0]?.height_inches}`;
+        rosterObj[playerNames[i]].id = playerId || 0;
+        rosterObj[playerNames[i]].position = position || "N/A";
+        rosterObj[playerNames[i]].weight = weight || "N/A";
+        rosterObj[playerNames[i]].height = height || "N/A";
 
-        playerId ? playerIds.push(playerId) : null
+        playerId ? playerIds.push(playerId) : null;
       }
 
       //Use array of playerIds to get player stats from balldontlie API
-      const idString = playerIds.join('&player_ids[]=', '')
+      const idString = playerIds.join("&player_ids[]=", "");
       await fetch(
         `https://www.balldontlie.io/api/v1/season_averages?season=${
           new Date().getFullYear() - 1
         }&player_ids[]=${idString}`
       )
-        .then(res => res.json())
-        .then(data => {
-          const playerStats = data.data
+        .then((res) => res.json())
+        .then((data) => {
+          const playerStats = data.data;
           for (let i = 0; i < playerStats.length; i++) {
             Object.entries(rosterObj).forEach(([key, value]) => {
               if (value.id === playerStats[i].player_id) {
-                rosterObj[key].stats = playerStats[i]
+                rosterObj[key].stats = playerStats[i];
               }
-            })
+            });
           }
         })
-        .catch(err => {
-          console.log(err)
-        })
-    }
-    await getPlayerIds()
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    await getPlayerIds();
 
-    updateRoster(rosterObj)
-  }
+    updateRoster(rosterObj);
+  };
 
-  await pushPlayerInfo()
+  await pushPlayerInfo();
 }
 
-runIndex()
+runIndex();
 
-getNews()
+getNews();
