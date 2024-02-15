5d881dca66c8b2e5dc04dc911cdf6fa0ab853f6f
Trey Gordon <41927921+treyg@users.noreply.github.com>
2024-02-15 14:16:20 -0500

    change port

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import { updateRoster, playerData, getNews } from "./firebase.js";
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Start the Express server
 app.listen(port, () => {
